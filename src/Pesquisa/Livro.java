136ffe834e12c9ba3497ef9f891f32aeec4c9817
Jessica Moreira <jessicamoreira51858@gmail.com>
2023-10-17 11:03:13 -0300

    Resolvendo desafios Java
    Signed-off-by: Jessica Moreira <jessicamoreira51858@gmail.com>

---
+++ src/Pesquisa/Livro.java
@@ -0,0 +1,35 @@
+package Pesquisa;
+
+public class Livro {
+    //atributos
+    private String titulo;
+    private String autor;
+    private int anoPublicação;
+
+    public Livro(String titulo, String autor, int anoPublicação) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicação = anoPublicação;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getAnoPublicação() {
+        return anoPublicação;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", anoPublicação=" + anoPublicação +
+                '}';
+    }
+}
